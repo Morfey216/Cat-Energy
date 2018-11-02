762726aa47de22116f9cff98df143de1e356187b
Morfey216 <Morfey4web@gmail.com>
2018-11-02 19:51:50 +0300

    Оптимизация растровых и векторных изображений

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,27 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass");
 var plumber = require("gulp-plumber");
+var imagemin = require("gulp-imagemin");
+var webp = require("gulp-webp");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
+
+gulp.task("images", function () {
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.jpegtran({progressive: true}),
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest("source/img"));
+});
+
+gulp.task("webp", function () {
+  return gulp.src("source/img/**/*.{png,jpg}")
+    .pipe(webp({quality: 80}))
+    .pipe(gulp.dest("source/img/webp"));
+});
 
 gulp.task("css", function () {
   return gulp.src("source/sass/style.scss")
